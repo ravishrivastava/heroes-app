26bdf5661c0206d0c6f7911a422d1489c486af91
Ravi Shrivastava <rsrivastava@ctepl.com>
2018-04-06 19:39:10 +0530

    added http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -9,6 +10,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
+import { InMemoryDataService } from './in-memory-data.service';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
@@ -24,9 +26,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
-  providers: [   HeroService, MessageService  ],
+  providers: [   HeroService, MessageService , InMemoryDataService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,23 +1,33 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+
+import { Hero } from './hero';
 import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+	private heroesUrl = 'api/heroes';  // URL to web api
 
-  getHeroes():  Observable<Hero[]> {
-  	this.messageService.add("Hero service : Fetched heroes")
-  	return of(HEROES);
+  constructor(
+  	private http: HttpClient,
+  	private messageService: MessageService) { }
+
+  getHeroes ():  Observable<Hero[]> {
+  	return this.http.get<Hero[]>(this.heroesUrl)
   }
 
-  getHero(id: number): Observable<Hero> {
-	  this.messageService.add(`HeroService: fetched hero id=${id}`);
-	  return of(HEROES.find(hero => hero.id === id));
-	}
+ //  getHero(id: number): Observable<Hero> {
+	//   this.messageService.add(`HeroService: fetched hero id=${id}`);
+	//   return of(HEROES.find(hero => hero.id === id));
+	// }
+
+	/** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
 
 }
